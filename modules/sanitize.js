82b3d7f94a991ce2bcaba227b8920719bd778418
tobilg <tobilg@gmail.com>
2019-07-24 10:44:31 +0200

    Also check for --stage flag

--- modules/sanitize.js
+++ modules/sanitize.js
@@ -11,6 +11,9 @@ module.exports.sanitizeDatabaseName = (serverless) => {
     if (!serverless.providers.aws.options.hasOwnProperty('domain')) {
         serverless.cli.log('No domain flag found, exiting! Please specify --domain <domainName>');
         process.exit(1);
+    } else if (!serverless.providers.aws.options.hasOwnProperty('stage')) {
+        serverless.cli.log('No stage flag found, exiting! Please specify --stage <stageName>');
+        process.exit(1);
     } else {
         return `ownstats_${serverless.providers.aws.options.domain.replace(/\./g, '_').replace(/-/g, '_')}_${serverless.providers.aws.options.stage}`
     }
